a95ecad9b55ab8ee656e39f7e9ce6cd6add45d61
Graydon Hoare <graydon@pobox.com>
2025-10-14 15:50:13 -0700

    disable more features

--- soroban-env-host/src/budget/wasmtime_helper.rs
+++ soroban-env-host/src/budget/wasmtime_helper.rs
@@ -1,3 +1,5 @@
+use wasmtime::wasmparser::WasmFeatures;
+
 use crate::{
     budget::{AsBudget, Budget},
     vm::SendHost,
@@ -80,12 +82,44 @@ pub(crate) fn get_wasmtime_config(_budget: &Budget) -> Result<wasmtime::Config, 
         .strategy(wasmtime::Strategy::Winch)
         .debug_info(false)
         .generate_address_map(false)
+        // Stuff we had enabled-or-disabled on wasmi.
         .consume_fuel(true)
         .wasm_bulk_memory(true)
+        .wasm_features(WasmFeatures::MUTABLE_GLOBAL, true)
+        .wasm_features(WasmFeatures::SIGN_EXTENSION, true)
+        .wasm_features(WasmFeatures::SATURATING_FLOAT_TO_INT, false)
         .wasm_multi_value(false)
-        // .wasm_reference_types(false) // cfg("gc")-compiled out entirely
+        .wasm_features(WasmFeatures::REFERENCE_TYPES, false)
         .wasm_tail_call(false)
         .wasm_extended_const(false)
-        .wasm_simd(false);
+        .wasm_features(WasmFeatures::FLOATS, false)
+        // Disable some more stuff while we're here, just to be sure.
+        .wasm_simd(false)
+        .wasm_relaxed_simd(false)
+        .wasm_features(WasmFeatures::THREADS, false)
+        .wasm_shared_everything_threads(false)
+        .wasm_multi_memory(false)
+        .wasm_features(WasmFeatures::EXCEPTIONS, false)
+        .wasm_memory64(false)
+        .wasm_features(WasmFeatures::COMPONENT_MODEL, false)
+        .wasm_features(WasmFeatures::FUNCTION_REFERENCES, false)
+        .wasm_features(WasmFeatures::MEMORY_CONTROL, false)
+        .wasm_features(WasmFeatures::GC, false)
+        .wasm_features(WasmFeatures::CUSTOM_PAGE_SIZES, false)
+        .wasm_features(WasmFeatures::LEGACY_EXCEPTIONS, false)
+        .wasm_features(WasmFeatures::GC_TYPES, false)
+        .wasm_features(WasmFeatures::STACK_SWITCHING, false)
+        .wasm_features(WasmFeatures::WIDE_ARITHMETIC, false)
+        .wasm_features(WasmFeatures::CM_VALUES, false)
+        .wasm_features(WasmFeatures::CM_NESTED_NAMES, false)
+        .wasm_features(WasmFeatures::CM_ASYNC, false)
+        .wasm_features(WasmFeatures::CM_ASYNC_STACKFUL, false)
+        .wasm_features(WasmFeatures::CM_ASYNC_BUILTINS, false)
+        .wasm_features(WasmFeatures::CM_THREADING, false)
+        .wasm_features(WasmFeatures::CM_ERROR_CONTEXT, false)
+        .wasm_features(WasmFeatures::CM_FIXED_SIZE_LIST, false)
+        .wasm_features(WasmFeatures::CM_GC, false)
+        .wasm_features(WasmFeatures::CALL_INDIRECT_OVERLONG, false);
+
     Ok(config)
 }
