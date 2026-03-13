stellar/rs-stellar-contract-host

Add wasmtime support and make wasmi optional.

soroban-env-host/src/vm/module_cache.rs
@@ -3,10 +3,10 @@ use super::{
     parsed_module::{CompilationContext, ParsedModule, VersionedContractCodeCostInputs},
 };
 use crate::{
-    budget::{get_wasmi_config, AsBudget, Budget},
+    budget::{get_wasmi_config, get_wasmtime_config, AsBudget, Budget},
     host::metered_clone::{MeteredClone, MeteredContainer},
     xdr::{Hash, ScErrorCode, ScErrorType},
-    Host, HostError, MeteredOrdMap,
+    ErrorHandler, Host, HostError, MeteredOrdMap,
 };
 use std::{
     collections::{BTreeMap, BTreeSet},
@@ -21,7 +21,9 @@ use std::{
 #[derive(Clone, Default)]
 pub struct ModuleCache {
     pub(crate) wasmi_engine: wasmi::Engine,
+    pub(crate) wasmtime_engine: wasmtime::Engine,
     pub(crate) wasmi_linker: wasmi::Linker<Host>,
+    pub(crate) wasmtime_linker: wasmtime::Linker<Host>,
     modules: ModuleCacheMap,
 }
 
@@ -106,17 +108,24 @@ impl ModuleCache {
         let wasmi_config = get_wasmi_config(host.as_budget())?;
         let wasmi_engine = wasmi::Engine::new(&wasmi_config);
 
+        let wasmtime_config = get_wasmtime_config(host.as_budget())?;
+        let wasmtime_engine = host.map_wasmtime_error(wasmtime::Engine::new(&wasmtime_config))?;
+
         let modules = ModuleCacheMap::MeteredSingleUseMap(MeteredOrdMap::new());
         let wasmi_linker = wasmi::Linker::new(&wasmi_engine);
+        let wasmtime_linker = wasmtime::Linker::new(&wasmtime_engine);
         let mut cache = Self {
             wasmi_engine,
+            wasmtime_engine,
             modules,
             wasmi_linker,
+            wasmtime_linker,
         };
 
         // Now add the contracts and rebuild linkers restricted to them.
         cache.add_stored_contracts(host)?;
         cache.wasmi_linker = cache.make_minimal_wasmi_linker_for_cached_modules(host)?;
+        cache.wasmtime_linker = cache.make_minimal_wasmtime_linker_for_cached_modules(host)?;
         Ok(cache)
     }
 
@@ -124,14 +133,21 @@ impl ModuleCache {
         let wasmi_config = get_wasmi_config(context.as_budget())?;
         let wasmi_engine = wasmi::Engine::new(&wasmi_config);
 
+        let wasmtime_config = get_wasmtime_config(context.as_budget())?;
+        let wasmtime_engine =
+            context.map_wasmtime_error(wasmtime::Engine::new(&wasmtime_config))?;
+
         let modules = ModuleCacheMap::UnmeteredReusableMap(Arc::new(Mutex::new(BTreeMap::new())));
 
         let wasmi_linker = Host::make_maximal_wasmi_linker(context, &wasmi_engine)?;
+        let wasmtime_linker = Host::make_maximal_wasmtime_linker(context, &wasmtime_engine)?;
 
         Ok(Self {
             wasmi_engine,
+            wasmtime_engine,
             modules,
             wasmi_linker,
+            wasmtime_linker,
         })
     }
 
@@ -244,6 +260,7 @@ impl ModuleCache {
             context,
             curr_ledger_protocol,
             &self.wasmi_engine,
+            &self.wasmtime_engine,
             &wasm,
             cost_inputs,
         )?;
@@ -297,6 +314,15 @@ impl ModuleCache {
     ) -> Result<wasmi::Linker<Host>, HostError> {
         self.with_minimal_import_symbols(host, |symbols| {
             Host::make_minimal_wasmi_linker_for_symbols(host, &self.wasmi_engine, symbols)
+        })
+    }
+
+    fn make_minimal_wasmtime_linker_for_cached_modules(
+        &self,
+        host: &Host,
+    ) -> Result<wasmtime::Linker<Host>, HostError> {
+        self.with_minimal_import_symbols(host, |symbols| {
+            Host::make_minimal_wasmtime_linker_for_symbols(host, &self.wasmtime_engine, symbols)
         })
     }
 

graydon/rs-soroban-env

soroban-env-host/src/vm/module_cache.rs
@@ -1,8 +1,8 @@
 use super::parsed_module::{CompilationContext, ParsedModule, VersionedContractCodeCostInputs};
 #[cfg(any(test, feature = "testutils"))]
 use crate::budget::AsBudget;
+use crate::vm::SendHost;
 use crate::{
-    budget::get_wasmi_config,
     host::metered_clone::MeteredClone,
     xdr::{Hash, ScErrorCode, ScErrorType},
     Host, HostError,
@@ -19,8 +19,14 @@ use std::{
 /// [Engine] is locked during execution and no new modules can be added to it.
 #[derive(Clone, Default)]
 pub struct ModuleCache {
+    #[cfg(feature = "wasmi")]
     pub(crate) wasmi_engine: wasmi::Engine,
-    pub(crate) wasmi_linker: wasmi::Linker<Host>,
+    #[cfg(feature = "wasmtime")]
+    pub(crate) wasmtime_engine: wasmtime::Engine,
+    #[cfg(feature = "wasmi")]
+    pub(crate) wasmi_linker: wasmi::Linker<SendHost>,
+    #[cfg(feature = "wasmtime")]
+    pub(crate) wasmtime_linker: wasmtime::Linker<SendHost>,
     modules: ModuleCacheMap,
 }
 
@@ -84,14 +90,32 @@ impl ModuleCacheMap {
 
 impl ModuleCache {
     pub fn new<Ctx: CompilationContext>(context: &Ctx) -> Result<Self, HostError> {
-        let wasmi_config = get_wasmi_config(context.as_budget())?;
+        #[cfg(feature = "wasmi")]
+        let wasmi_config = crate::budget::get_wasmi_config(context.as_budget())?;
+        #[cfg(feature = "wasmi")]
         let wasmi_engine = wasmi::Engine::new(&wasmi_config);
+
+        #[cfg(feature = "wasmtime")]
+        let wasmtime_config = crate::budget::get_wasmtime_config(context.as_budget())?;
+        #[cfg(feature = "wasmtime")]
+        let wasmtime_engine =
+            context.map_wasmtime_error(wasmtime::Engine::new(&wasmtime_config))?;
+
         let modules = ModuleCacheMap::default();
+        #[cfg(feature = "wasmi")]
         let wasmi_linker = Host::make_maximal_wasmi_linker(context, &wasmi_engine)?;
+        #[cfg(feature = "wasmtime")]
+        let wasmtime_linker = Host::make_maximal_wasmtime_linker(context, &wasmtime_engine)?;
         Ok(Self {
+            #[cfg(feature = "wasmi")]
             wasmi_engine,
+            #[cfg(feature = "wasmtime")]
+            wasmtime_engine,
             modules,
+            #[cfg(feature = "wasmi")]
             wasmi_linker,
+            #[cfg(feature = "wasmtime")]
+            wasmtime_linker,
         })
     }
 
@@ -172,7 +196,10 @@ impl ModuleCache {
         let parsed_module = ParsedModule::new(
             context,
             curr_ledger_protocol,
+            #[cfg(feature = "wasmi")]
             &self.wasmi_engine,
+            #[cfg(feature = "wasmtime")]
+            &self.wasmtime_engine,
             &wasm,
             cost_inputs,
         )?;